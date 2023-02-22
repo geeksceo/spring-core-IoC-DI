5918b3d629c62aa3a9e5e1d053ffc08f60f22917
geeksceo <oowashina@gmail.com>
2023-02-22 17:21:00 +0000

    Dare mighty thins - Inversion of Control and Dependency injection - First stage of things - for absolute beginners like me ath this time.

---
+++ src/com/journaldev/java/legacy/MyApplication.java
@@ -0,0 +1,12 @@
+package com.journaldev.java.legacy;
+
+public class MyApplication {
+
+	private EmailService email = new EmailService();
+	
+	public void processMessages(String msg, String rec) {
+		// do some message validation, manipulation logic etc
+		this.email.sendEmail(msg, rec);
+	}
+	
+}
